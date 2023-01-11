3ad89bdf395aa1015e7e7bed85df36c3ce260d21
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-11 16:00:16 -0600

    videogame start

---
+++ main.js
@@ -0,0 +1,42 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+
+c.fillRect(0,0, canvas.width, canvas.height);
+
+class Sprite {
+    constructor({position, velocity}) {
+    this.position = position;
+    this.velocity = velocity;
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, 150);
+
+    }
+}
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    velocity: {
+        x:0,
+        y:0
+    }
+})
+player.draw();
+
+const enemy = new Sprite({
+    x: 400,
+    y: 400,
+});
+
+enemy.draw();
+console.log(player);
+function animate(){
+    window.requestAnimationFrame(animate)
+}
