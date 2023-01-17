0782755ed31de71f6c612c9333f576f3f633ee02
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-17 17:27:15 -0600

    changing gravity and velocity for players

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ canvas.height = 576;
 
 
 c.fillRect(0,0, canvas.width, canvas.height);
-const gravity = 0.2;
+const gravity = 0.7;
 class Sprite {
     constructor({position, velocity}) {
     this.position = position;
@@ -72,7 +72,6 @@ const keys = {
     }
 
 }
-let lastKey;
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -84,15 +83,15 @@ function animate(){
     player.velocity.x = 0;
     enemy.velocity.x = 0;
 //player movement
-    if (keys.a.pressed && lastKey === 'a') {
-        player.velocity.x = -1;
-    } else if(keys.d.pressed && lastKey === 'd') {
-        player.velocity.x = 1;
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -5;
+    } else if(keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x = 5;
         //enemy movement
     }  if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
-        enemy.velocity.x = -1;
+        enemy.velocity.x = -5;
     } else if(keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
-        enemy.velocity.x = 1;
+        enemy.velocity.x = 5;
     }
 }
 animate();
@@ -102,14 +101,14 @@ animate();
         switch (event.key) {
             case 'd':
                 keys.d.pressed = true;
-                lastKey = 'd';
+                player.lastKey = 'd';
                 break;
             case 'a':
                 keys.a.pressed = true
-                lastKey = 'a'
+                player.lastKey = 'a'
                 break;
          case 'w':
-            player.velocity.y = -10
+            player.velocity.y = -20
                 break;
             //enemy keys
             case 'ArrowRight':
@@ -121,7 +120,7 @@ animate();
                 enemy.lastKey = 'ArrowLeft'
                 break;
          case 'ArrowUp':
-            enemy.velocity.y = -10
+            enemy.velocity.y = -20
                 break
         }
         console.log(event.key)
