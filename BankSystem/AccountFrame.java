624623dfa3fbe5e5f12f5f5aa7cf4103fcbdec6a
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-19 22:31:27 -0600

    adding buttons

--- BankSystem/AccountFrame.java
+++ BankSystem/AccountFrame.java
@@ -2,6 +2,7 @@ package BankSystem;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.TreeSet;
@@ -38,9 +39,51 @@ public class AccountFrame extends JFrame {
         setSize(600, 400);
 
         //Adding components to the Frame
+        //1 - Labels
         accnNoLBL = new JLabel("Account No.");
         ownerLBL = new JLabel("Owner");
         balanceLBL = new JLabel("balance");
+        cityLBL = new JLabel("City");
+        genderLBL = new JLabel("Gender");
+        amountLBL = new JLabel("Amount");
+
+        //2 - TextFields
+        accNoTXT = new JTextField(); accNoTXT.setEnabled(false);
+        ownerTXT = new JTextField();
+        balanceTXT = new JTextField(); balanceTXT.setEnabled(false);
+        amountTXT = new JTextField();
+        amountTXT.setPreferredSize(new Dimension(150, 25));
+
+        //ComboBox
+        citiesCMBMDL = new DefaultComboBoxModel<>();
+        citiesCMBMDL.addElement(null);
+        citiesCMBMDL.addElement(new City("Brooklyn", "New York"));
+        citiesCMBMDL.addElement(new City("Minneapolis", "Minnesota"));
+        citiesCMBMDL.addElement(new City("Chicago", "Illinois"));
+        citiesCMBMDL.addElement(new City("Miami", "Florida"));
+        citiesCMBMDL.addElement(new City("San Diego", "California"));
+
+        //Adding data to JCOMBOBOX
+        citiesCMB = new JComboBox<City>(citiesCMBMDL);
+
+        // 4 - Radio Buttons
+        maleRDB = new JRadioButton("Male", true);
+        femaleRDB = new JRadioButton("Female");
+        genderBTNGRP = new ButtonGroup();
+        genderBTNGRP.add(maleRDB);
+        genderBTNGRP.add(femaleRDB);
+
+        // 5 - Buttons
+        newBTN = new JButton("New");
+        saveBTN = new JButton("Save");
+        showBTN = new JButton("Show");
+        quitBTN = new JButton("Quit");
+        depositBTN = new JButton("Deposit");
+        withdrawBTN = new JButton("Withdraw");
+
+
+
+
 
     }
 
